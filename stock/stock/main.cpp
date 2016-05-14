9c73c2010beeb61dc7a1a7848adef83f0411366e
Buczacka Walter <wbuczacka@deviget.com>
2016-05-14 11:30:49 -0300

    untrack files

---
+++ stock/stock/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  stock
+//
+//  Created by Walter Buczacka on 5/14/16.
+//  Copyright © 2016 Walter Buczacka. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
