6af3ed3376e1bb686c4993780f205b1177ae63fd
Buczacka Walter <wbuczacka@deviget.com>
2016-05-15 15:15:39 -0300

    agregar funciones tda articulo

--- stock/stock/articulo.cpp
+++ stock/stock/articulo.cpp
@@ -6,6 +6,7 @@
 //  Copyright © 2016 Walter Buczacka. All rights reserved.
 //
 
+#include "articulo.h"
 #include <fstream>
 #include <iostream>
 #include <sstream>
@@ -15,16 +16,23 @@ using namespace std;
 
 void cargarArchivo(const char ruta[]) {
     
-    int id;
+    int codigo;
     float precio;
     char descripcion[100]; //no lo uso todavia
 
     FILE *fin = fopen(ruta, "r");
     
     if (fin != NULL) {
-        while (fscanf(fin,"%d;%f", &id, &precio) > 0) {
+        while (fscanf(fin,"%d;%f", &codigo, &precio) > 0) {
             
-            cout << id << " "  << precio ;
+            cout << codigo << " "  << precio ;
+            //puedo tener varios constructores y pasar directamente?
+            Articulo articulo;
+            setCodigoArticulo(articulo, codigo);
+            setPrecioArticulo(articulo, precio);
+            
+            //
+            
         
         }
         
@@ -35,6 +43,8 @@ void cargarArchivo(const char ruta[]) {
     }
 }
 
+
+
 void cargarMaestroDeArtículos() {
 
     const char ruta[] = "/Users/walter/Documents/personal-repositories/stock/stock/stock/articulos.db";
@@ -43,3 +53,99 @@ void cargarMaestroDeArtículos() {
 }
 
 
+/**
+ PRE:
+ La instancia del TDA (circulo) no debe haberse creado (crear) ni destruido (destruir) con anterioridad.
+ POST:
+ Deja la instancia del TDA (circulo) listo para ser usado. El valor por defecto del radio es 1.
+ ATRIBUTOS:
+ articulo: instancia sobre la cual se aplica la primitiva.
+ RETORNO:
+ No aplica.
+ */
+
+void constructor(Articulo &articulo){}
+
+/**
+ PRE:
+ La instancia del TDA (articulo) no debe haberse creado ni destruido  con anterioridad.
+ POST:
+ Deja la instancia del TDA (articulo) listo para ser usado.
+ ATRIBUTOS:
+ articulo: instancia sobre la cual se aplica la primitiva.
+ RETORNO:
+ No aplica.
+ */
+
+void destructior(Articulo &articulo){}
+
+
+/**
+ PRE:
+ La instancia del TDA (articulo) debe haberse creado (crear) pero no debe estar destruida (destruir).
+ POST:
+ Devuelve el codigo del articulo.
+ ATRIBUTOS:
+ articulo: instancia sobre la cual se aplica la primitiva.
+ RETORNO:
+ Devuelve el codigo del articulo.
+ */
+int getCodigoArticulo(Articulo &articulo){
+    return articulo.codigoArticulo;
+}
+
+
+/**
+ PRE:
+ La instancia del TDA (articulo) debe haberse creado (crear) pero no debe estar destruida (destruir).
+ El codigo debe ser mayor o igual a cero.
+ POST:
+ El articulo queda con el nuevo codigo.
+ ATRIBUTOS:
+ articulo: instancia sobre la cual se aplica la primitiva.
+ codigo: valor del codigo a asignar al articulo.
+ RETORNO:
+ No aplica.
+ */
+void setCodigoArticulo(Articulo &articulo, int codigo){
+    // Agrego robustez aunque estoy protegido con la pre.
+    if (codigo >= 0) {
+        articulo.codigoArticulo = codigo;
+    }
+}
+
+/**
+ PRE:
+ La instancia del TDA (articulo) debe haberse creado (crear) pero no debe estar destruida (destruir).
+ POST:
+ Devuelve el precio del articulo.
+ ATRIBUTOS:
+ articulo: instancia sobre la cual se aplica la primitiva.
+ RETORNO:
+ Devuelve el precio del articulo.
+ */
+float getpPrecioArticulo(Articulo &articulo){
+    return articulo.precio;
+}
+
+
+/**
+ PRE:
+ La instancia del TDA (articulo) debe haberse creado (crear) pero no debe estar destruida (destruir).
+ El codigo debe ser mayor o igual a cero.
+ POST:
+ El articulo queda con el nuevo precio.
+ ATRIBUTOS:
+ articulo: instancia sobre la cual se aplica la primitiva.
+ precio: valor del codigo a asignar al articulo.
+ RETORNO:
+ No aplica.
+ */
+void setCodigoArticulo(Articulo &articulo, float precio){
+    // Agrego robustez aunque estoy protegido con la pre.
+    if (precio > 0) {
+        articulo.precio = precio;
+    }
+}
+
+//falta descripcion
--- stock/stock/articulo.h
+++ stock/stock/articulo.h
@@ -23,3 +23,86 @@ struct Articulo {
 void cargarMaestroDeArtículos();
 
 void cargarArchivo(const char ruta[]);
+
+/**
+ PRE:
+ La instancia del TDA (circulo) no debe haberse creado (crear) ni destruido (destruir) con anterioridad.
+ POST:
+ Deja la instancia del TDA (circulo) listo para ser usado. El valor por defecto del radio es 1.
+ ATRIBUTOS:
+ articulo: instancia sobre la cual se aplica la primitiva.
+ RETORNO:
+ No aplica.
+ */
+
+void constructor(Articulo &articulo);
+
+/**
+ PRE:
+ La instancia del TDA (articulo) no debe haberse creado ni destruido  con anterioridad.
+ POST:
+ Deja la instancia del TDA (articulo) listo para ser usado.
+ ATRIBUTOS:
+ articulo: instancia sobre la cual se aplica la primitiva.
+ RETORNO:
+ No aplica.
+ */
+
+void destructior(Articulo &articulo);
+
+
+/**
+ PRE:
+ La instancia del TDA (articulo) debe haberse creado (crear) pero no debe estar destruida (destruir).
+ POST:
+ Devuelve el codigo del articulo.
+ ATRIBUTOS:
+ articulo: instancia sobre la cual se aplica la primitiva.
+ RETORNO:
+ Devuelve el codigo del articulo.
+ */
+int getCodigoArticulo(Articulo &articulo);
+
+
+/**
+ PRE:
+ La instancia del TDA (articulo) debe haberse creado (crear) pero no debe estar destruida (destruir).
+ El codigo debe ser mayor o igual a cero.
+ POST:
+ El articulo queda con el nuevo codigo.
+ ATRIBUTOS:
+ articulo: instancia sobre la cual se aplica la primitiva.
+ codigo: valor del codigo a asignar al articulo.
+ RETORNO:
+ No aplica.
+ */
+void setCodigoArticulo(Articulo &articulo, int codigo);
+
+/**
+ PRE:
+ La instancia del TDA (articulo) debe haberse creado (crear) pero no debe estar destruida (destruir).
+ POST:
+ Devuelve el precio del articulo.
+ ATRIBUTOS:
+ articulo: instancia sobre la cual se aplica la primitiva.
+ RETORNO:
+ Devuelve el precio del articulo.
+ */
+float getPrecioArticulo(Articulo &articulo);
+
+
+/**
+ PRE:
+ La instancia del TDA (articulo) debe haberse creado (crear) pero no debe estar destruida (destruir).
+ El codigo debe ser mayor o igual a cero.
+ POST:
+ El articulo queda con el nuevo precio.
+ ATRIBUTOS:
+ articulo: instancia sobre la cual se aplica la primitiva.
+ precio: valor del codigo a asignar al articulo.
+ RETORNO:
+ No aplica.
+ */
+void setPrecioArticulo(Articulo &articulo, float precio);
+
+//falta descripcion
