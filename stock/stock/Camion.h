637f0a01930a92e6852173f7488b56e083f5748d
Dario <dariozenden@gmail.com>
2016-05-17 21:18:15 -0300

    agrego archivo camion.cpp

---
+++ stock/stock/Camion.h
@@ -0,0 +1,207 @@
+#include <cstdlib>;
+#include <iostream>;
+#include <string>;
+
+using namespace std;
+
+#ifndef __CAMION_H__
+#define __CAMION_H__
+#ifndef NULL
+#define NULL 0
+#endif
+
+/******************************************************************************/
+
+/* Definiciones de Tipos de Datos */
+
+/*-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+/* Tipo de Informacion que esta contenida en una plataforma*/
+
+typedef struct{	
+	int nroCamion;
+	float kgCapacidad;
+	float cargaTotal;
+	float porcentajeOcupado;	
+	Pila pilaSolicitudes;	
+	
+} Camion;
+
+/******************************************************************************/
+
+/* Definicion de Primitivas */
+
+/**
+PRE:
+    La instancia del TDA (camion) no debe haberse creado ni destruido con anterioridad.
+POST:
+    Deja la instancia del TDA (camion) listo para ser usado.
+ATRIBUTOS:
+    camion: instancia sobre la cual se aplica la primitiva.
+RETORNO:
+    No aplica.
+*/
+
+void crearCamion(Camion &camion);
+
+
+/**
+PRE:
+    La instancia del TDA (camion) debe haberse creado con anterioridad y nunca se ejecutó la primitiva destruir sobre esta instancia.
+POST:
+    Destruye la instancia del TDA y ya no podrá ser utilizada.
+ATRIBUTOS:
+    camion: instancia sobre la cual se aplica la primitiva.
+RETORNO:
+    No aplica.
+*/
+
+void destruirCamion(Camion &camion);
+
+
+/**
+PRE:
+    La instancia del TDA (camion) debe haberse creado con anterioridad y nunca se ejecutó la primitiva destruir sobre esta instancia.
+POST:
+    El campo nroCamion pasa a tener el dato ingresado.
+PARAMETROS:
+    camion: instancia sobre la cual se aplica la primitiva.
+    dato: valor utilizado para asignar a la instancia de camion.
+RETORNO:
+    No aplica.
+*/
+
+void setNroCamion(Camion &camion, int dato);
+
+
+/**
+PRE:
+    La instancia del TDA (camion) debe haberse creado con anterioridad y nunca se ejecutó la primitiva destruir sobre esta instancia.
+POST:
+    El campo koCapacidad pasa a tener el dato ingresado.
+PARAMETROS:
+    camion: instancia sobre la cual se aplica la primitiva.
+    dato: valor utilizado para asignar a la instancia de camion.
+RETORNO:
+    No aplica.
+*/
+
+void setKGcapacidad(Camion &camion, float dato);
+
+
+/**
+PRE:
+    La instancia del TDA (camion) debe haberse creado con anterioridad y nunca se ejecutó la primitiva destruir sobre esta instancia.
+POST:
+    El campo cargaTotal pasa a tener el dato ingresado.
+PARAMETROS:
+    camion: instancia sobre la cual se aplica la primitiva.
+    dato: valor utilizado para asignar a la instancia de camion.
+RETORNO:
+    No aplica.
+*/
+
+void setCargaTotal(Camion &camion, float dato);
+
+
+/**
+PRE:
+    La instancia del TDA (camion) debe haberse creado con anterioridad y nunca se ejecutó la primitiva destruir sobre esta instancia.
+POST:
+    El campo porcentajeOcupado pasa a tener el dato ingresado.
+PARAMETROS:
+    camion: instancia sobre la cual se aplica la primitiva.
+    dato: valor utilizado para asignar a la instancia de camion.
+RETORNO:
+    No aplica.
+*/
+
+void setPorcentajeOcupado(Camion &camion, Pila dato);
+
+
+/**
+PRE:
+    La instancia del TDA (camion) debe haberse creado con anterioridad y nunca se ejecutó la primitiva destruir sobre esta instancia.
+POST:
+    Actualizo pilaSolicitudes asignandole pilaNueva
+PARAMETROS:
+    camion: instancia sobre la cual se aplica la primitiva.
+    pilaNueva: Actualizo la pilaSolicitudes.
+RETORNO:
+    No aplica.
+*/
+
+void setPilaSolicitudes(Camion &camion, Pila pilaNueva);
+
+
+/**
+PRE:
+    La instancia del TDA (camion) debe haberse creado con anterioridad (primitiva crear) y nunca se ejecutó la primitiva destruir sobre esta instancia.
+POST:
+    devuelve el Numero de Camion
+ATRIBUTOS:
+    camion: instancia sobre la cual se aplica la primitiva.
+RETORNO:
+    Numero de camion en formato int.
+*/
+
+int getNroCamion(Camion &camion);
+
+
+/**
+PRE:
+    La instancia del TDA (camion) debe haberse creado con anterioridad (primitiva crear) y nunca se ejecutó la primitiva destruir sobre esta instancia.
+POST:
+    devuelve la Capicidad en Kilogramos
+ATRIBUTOS:
+    camion: instancia sobre la cual se aplica la primitiva.
+RETORNO:
+    Capicidad en Kilogramos en formato float.
+*/
+
+float getKGcapacidad(Camion &camion);
+
+
+/**
+PRE:
+    La instancia del TDA (camion) debe haberse creado con anterioridad (primitiva crear) y nunca se ejecutó la primitiva destruir sobre esta instancia.
+POST:
+    devuelve la Carga total de kilogramos
+ATRIBUTOS:
+    camion: instancia sobre la cual se aplica la primitiva.
+RETORNO:
+    Carga total de kilogramos en formato float.
+*/
+
+float getCargaTotal(Camion &camion);
+
+
+/**
+PRE:
+    La instancia del TDA (camion) debe haberse creado con anterioridad (primitiva crear) y nunca se ejecutó la primitiva destruir sobre esta instancia.
+POST:
+    devuelve el porcentaje ocupado del camion
+ATRIBUTOS:
+    camion: instancia sobre la cual se aplica la primitiva.
+RETORNO:
+   	Porcentaje ocupado del camion en formato float.
+*/
+
+float getPorcentajeOcupado(Camion &camion);
+
+
+/**
+PRE:
+    La instancia del TDA (camion) debe haberse creado con anterioridad (primitiva crear) y nunca se ejecutó la primitiva destruir sobre esta instancia.
+POST:
+    devuelve la pila de solicitudes
+ATRIBUTOS:
+    camion: instancia sobre la cual se aplica la primitiva.
+RETORNO:
+   	Pila de solicitudes.
+*/
+
+Pila getPorcentajeOcupado(Camion &camion);
+
+
+#endif
