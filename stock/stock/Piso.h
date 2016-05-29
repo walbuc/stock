c93976d40f22c0e4c10c2659eb428e174d007b90
gonzalezgustavo <gonzalezgustavojavier@gmail.com>
2016-05-29 12:38:25 -0300

    agregue Piso.h

---
+++ stock/stock/Piso.h
@@ -0,0 +1,30 @@
+#ifndef PISO_H
+#define PISO_H
+#include "Lista.h"
+#include "Ubicacion.h"
+
+
+typedef struct{
+    int nroPiso;
+    Lista listaUbicacion;
+
+} Piso;
+
+
+/*funciones extras*/
+
+void construirPiso(Piso &piso);
+
+int getNroPisos(Piso &piso);
+
+void setNroPisos(Piso &piso, int nro);
+
+Lista getListaUbicacion(Piso &piso);
+
+void setListaUbicacion(Piso &piso, Lista listaUbicacion);
+
+
+
+
+
+#endif // PISO_H
