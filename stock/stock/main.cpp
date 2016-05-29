5788bd75b67e5a64de270aa2b7dce08dc9f0df4b
gonzalezgustavo <gonzalezgustavojavier@gmail.com>
2016-05-29 13:03:53 -0300

    agregue cambios en el main

--- stock/stock/main.cpp
+++ stock/stock/main.cpp
@@ -9,9 +9,12 @@
 #include <iostream>
 #include "AdminArticulos.h"
 #include "AdminEstanteria.h"
+#include "estanteria.h"
+#include "Piso.h"
+#include "Ubicacion"
 #include "lista.h"
 #include "funciones.h"
-#include "estanteria.h"
+
 #include "Articulo.h"
 #include "AdminOperaciones.h"
 
@@ -27,14 +30,40 @@ int main(int argc, const char * argv[]) {
     
     cargarMaestroDeArticulos(listaArticulo, listaIndice);
     
-    Estanteria estanteria;
-    //construirEstanteria(estanteria);
-    
-    //configurarEstanteria(estanteria);
-    //imprime el nro de calle, cantidad de pisos y ubicaciones, y Kilos por camion
-    //imprimirEstanteria(estanteria);
     
     leerArchivosOperaciones();
+    
+    /*Prueba de las estanterias*/
+     /*creo estanteria*/
+    Estanteria estanteria;
+    construirEstanteria(estanteria);
+    configurarEstanteria(estanteria);//Levanto los parametros de .confg
+
+    setNroCalle(estanteria,1);//Seteo el nro de calle
+
+    /*Creo tres articulos para probar*/
+    Articulo articulo;
+    constructor(articulo);
+    setCodigoArticulo(articulo,1);
+    Articulo articulo2;
+    constructor(articulo2);
+    setCodigoArticulo(articulo2,2);
+    Articulo articulo3;
+    constructor(articulo3);
+    setCodigoArticulo(articulo3,3);
+    
+    /*Creo un solo piso para probar*/
+    crearPiso(estanteria,1);
+   
+   /*creo las 3 ubicaciones en el piso creado*/
+    crearUbicacion(estanteria,1,articulo);
+    crearUbicacion(estanteria,2,articulo2);
+    crearUbicacion(estanteria,3,articulo3);
+    
+    /*Imprimo la estanteria completa, Pero no lee bien la listaUbicaciones*/
+    imprimirEstanteria(estanteria);
+ 
+ 
     
     
     cout << "Hello, World!\n";
