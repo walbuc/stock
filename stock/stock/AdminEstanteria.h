8a2b44b88fc56ba4eb0c9afccbf70f51b7e19dcb
gonzalezgustavo <gonzalezgustavojavier@gmail.com>
2016-05-29 12:44:51 -0300

    agregue cambios en AdminEstanteria.h

--- stock/stock/AdminEstanteria.h
+++ stock/stock/AdminEstanteria.h
@@ -6,14 +6,16 @@
 #include "Pila.h"
 #include "Estanteria.h"
 #include "Funciones.h"
+#include "Ubicacion.h"
+#include "Piso.h"
 
 void configurarEstanteria(Estanteria &estanteria);
 
-void estadoUbicacion(Pila &pilaUbicacion, int piso);
+void estadoUbicacion(Lista &listaUbicacion, int piso);
 
 void estadoPiso(Lista &listaPiso);
 
-void imprimirUbicacion(Ubicacion ubicacion);
+void imprimirUbicacion(Ubicacion &ubicacion);
 
 void imprimirEstanteria(Estanteria &estanteria);
 
