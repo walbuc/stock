9ca54275ae225ab9ce2c2a8177eb0db03b16750c
gonzalezgustavo <gonzalezgustavojavier@gmail.com>
2016-05-29 23:25:53 -0300

    agrego AdminDeposito.cpp

---
+++ stock/stock/AdminDeposito.cpp
@@ -0,0 +1,52 @@
+#include "AdminDeposito.h"
+#include "Funciones.h"
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <stdio.h>
+
+using namespace std;
+
+void configurarDeposito(Deposito &deposito){
+
+    ifstream fdeposito;
+    string dato;
+
+    fdeposito.open("configuracion.conf");
+
+    if (fdeposito.is_open()){
+
+        while (fdeposito.good()){
+            getline(fdeposito,dato);
+            setCPisos(deposito, cadenaAentero(dato));
+            getline (fdeposito, dato);
+            setCUbicaciones(deposito, cadenaAentero(dato));
+            getline (fdeposito,dato);
+            setKc(deposito,cadenaAfloat(dato));
+       }
+
+        fdeposito.close();
+
+    }
+
+    else{
+
+        cout << "Error al abrir el archivo." << endl << endl;
+
+// system ("PAUSE");
+
+    }
+}
+
+void imprimirDeposito(Deposito &deposito){
+    cout<<"--------------------------------------"<<endl;
+    cout<<"Nro Deposito: "<<deposito.nroDeposito<<endl;
+    cout<<"Cantidad de Pisos: "<<deposito.cPisos<<endl;
+    cout<<"Cantidad de Ubicaciones: "<<deposito.cUbicaciones<<endl;
+    cout<<"Kilos por Camion: "<<deposito.kc<<endl;
+    cout<<"--------------------------------------"<<endl;
+
+
+
+}
