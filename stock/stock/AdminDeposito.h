7e4e7ae3d658404dfefd34cf8eaef1120f1bc988
gonzalezgustavo <gonzalezgustavojavier@gmail.com>
2016-05-29 23:26:09 -0300

    agrego AdminDeposito.h

---
+++ stock/stock/AdminDeposito.h
@@ -0,0 +1,8 @@
+#ifndef ADMINDEPOSITO_H
+#define ADMINDEPOSITO_H
+#include "Deposito.h"
+
+void configurarDeposito(Deposito &deposito);
+void imprimirDeposito(Deposito &deposito);
+
+#endif // ADMINDEPOSITO_H
