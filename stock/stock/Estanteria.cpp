83dbe5d3d39e24599cfbb008343cacadb7494bf7
gonzalezgustavo <gonzalezgustavojavier@gmail.com>
2016-05-29 23:12:33 -0300

    cambios en Estanteria.cpp

--- stock/stock/Estanteria.cpp
+++ stock/stock/Estanteria.cpp
@@ -19,24 +19,17 @@ using namespace std;
  RETORNO:
  No aplica.
  **/
-void construirEstanteria (Estanteria &estanteria){
+PtrDato construirEstanteria (Estanteria &estanteria){
 
-
-     /*numero de calle es 0*/
+     PtrDato puntero= new Estanteria();
      estanteria.nroCalle=0;
      /*Setea la listaPisos, Ubicacion no tendria, no sería necesario inicializar*/
-     estanteria.cPisos=0;
-     estanteria.cUbicaciones=0;
-     estanteria.kc=0;
-
      Lista auxLista;
-     crearLista(auxLista,compararDatoPiso);
-     setListaPisos(estanteria,auxLista);
-     auxLista= getListaPisos(estanteria);
-     cout<<"constructor estanteria "<<longitud(auxLista)<<endl;//lo tengo para ver si crea la lista vacia
+     crearLista(auxLista,&compararDatoPiso);
+     estanteria.listaPisos=auxLista;
 
+return puntero;
 }
-
 
 /**
  PRE:
@@ -79,100 +72,7 @@ void construirEstanteria (Estanteria &estanteria){
  }
 
 
- /**
- GET_cPisos
- PRE:
- La instancia del TDA (estanteria) debe haberse creado (crear) ni destruido (destruir) con anterioridad.
- POST:Devuelve el numero cantidad de pisos.
- ATRIBUTOS:
- Estanteria: instancia sobre la cual se aplica la primitiva.
- RETORNO:
- Devuelve el numero de cPisos..
- **/
-int getCPisos(Estanteria &estanteria){
-    return estanteria.cPisos;
-}
-
-
- /**
- SET_cPisos
- PRE:
- La instancia del TDA (estanteria) debe haberse creado (crear) ni destruido (destruir) con anterioridad.
- POST:Cambia el numero de calle.
- ATRIBUTOS:
- Estanteria: instancia sobre la cual se aplica la primitiva.
- cPisos: cantidad de pisos. TOPE.
- RETORNO:
- no aplica.
- **/
-void setCPisos(Estanteria &estanteria, int cPisos){
-    estanteria.cPisos= cPisos;
-}
-
-
 /**
- GET_cUbicaciones
- PRE:
- La instancia del TDA (estanteria) debe haberse creado (crear) ni destruido (destruir) con anterioridad.
- POST:Devuelve el numero de Ubicaciones.
- ATRIBUTOS:
- Estanteria: instancia sobre la cual se aplica la primitiva.
- RETORNO:
- Devuelve el numero de cUbicaciones..
- **/
- int getCUbicaciones(Estanteria &estanteria){
-    return estanteria.cUbicaciones;
- }
-
-
- /**
- SET_cUbicaciones
- PRE:
- La instancia del TDA (estanteria) debe haberse creado (crear) ni destruido (destruir) con anterioridad.
- POST:Cambia el numero de cantidad de ubicaciones. TOPE
- ATRIBUTOS:
- Estanteria: instancia sobre la cual se aplica la primitiva.
- cUbicaciones
- RETORNO:
- no aplica.
- **/
- void setCUbicaciones(Estanteria &estanteria, int cUbicaciones){
-    estanteria.cUbicaciones=cUbicaciones;
- }
-
-
- /**
- GET_KC
- PRE:
- La instancia del TDA (estanteria) debe haberse creado (crear) ni destruido (destruir) con anterioridad.
- POST:Devuelve el numero de kc. Kilos por camion
- ATRIBUTOS:
- Estanteria: instancia sobre la cual se aplica la primitiva.
- RETORNO:
- Devuelve el numero de kc..
- **/
- float getKc (Estanteria &estanteria){
-     return estanteria.kc;
- }
-
-
- /**
- SET_NUMERO_CALLE
- PRE:
- La instancia del TDA (estanteria) debe haberse creado (crear) ni destruido (destruir) con anterioridad.
- POST:Devuelve el numero de calle.
- ATRIBUTOS:
- Estanteria: instancia sobre la cual se aplica la primitiva.
- kc: numero a setear en KC.
- RETORNO:
- No aplica.
- **/
- void setKc(Estanteria &estanteria, float kc){
-    estanteria.kc= kc;
- }
-
-
- /**
  PRE:
  La instancia del TDA (estanteria) debe haberse creado (crear) ni destruido (destruir) con anterioridad.
  POST:Devuelve Lista de Pisos.
@@ -181,8 +81,8 @@ void setCPisos(Estanteria &estanteria, int cPisos){
  RETORNO:
  Devuelve Lista de Pisos.
  **/
-Lista getListaPisos(Estanteria &estanteria){
-    return estanteria.listaPisos;
+PtrDato getListaPisos(Estanteria &estanteria){
+    return &estanteria.listaPisos;
  }
 
 
@@ -212,31 +112,15 @@ Lista getListaPisos(Estanteria &estanteria){
  RETORNO:
  No aplica. Solo se crea un piso con los parametros pasados.
  **/
- void crearPiso(Estanteria &estanteria, int nroPiso){
+
+
+ void crearPiso(Lista &listaPisos, int nroPiso){
     //Piso a Crear
     Piso piso;
-    construirPiso(piso);
-    piso.nroPiso=nroPiso;
-
-    //Crear lista auxiliar para los pisos
-    Lista auxLista;
-    crearLista(auxLista, compararDatoPiso);
-    auxLista= getListaPisos(estanteria);
-
-    //revisa si esta vacia la lista para adicionar al principio o al final
-    if (listaVacia(auxLista)){
-        adicionarPrincipio(auxLista, &piso);
-        cout<<"piso nro "<< piso.nroPiso<<endl;
-        setListaPisos(estanteria,auxLista);
-         }
-         else {
-            adicionarFinal(auxLista, &piso);
-            cout<<"final"<<piso.nroPiso<<endl;
-            setListaPisos(estanteria,auxLista);
-         }
-
-    }
-
+    PtrDato ptrDato= construirPiso(piso);
+    setNroPisos(*((Piso*)ptrDato),nroPiso);
+    adicionarFinal(listaPisos,ptrDato);
+   }
 
 /**
  CREAR_UBICACION
@@ -250,65 +134,34 @@ Lista getListaPisos(Estanteria &estanteria){
  RETORNO:
  No aplica. Solo se crea la ubicacion.
  **/
-void crearUbicacion(Estanteria &estanteria, int nroUbicacion, Articulo &articulo){
+void crearUbicacion(Lista &listaUbicacion, int nroUbicacion, Articulo &articulo){
 
-    /*crea ubicacion para apilar*/
     Ubicacion ubicacion;
-    construirUbicacion(ubicacion);
-    setNroUbicacion(ubicacion, nroUbicacion);
-    setArticulo(ubicacion, articulo);
+    PtrDato ptrDato= construirUbicacion(ubicacion);
 
-    Lista auxListaPisos;
-    crearLista(auxListaPisos, compararDatoPiso);
-    auxListaPisos=getListaPisos(estanteria);
+    setNroUbicacion(*((Ubicacion*)ptrDato),nroUbicacion);
+    setArticulo(*((Ubicacion*)ptrDato),articulo);
+    adicionarFinal(listaUbicacion, ptrDato);
 
-    cout<<"longitud auxListaPisos: "<<longitud(auxListaPisos)<<endl;
-
-
-    /*saco el ultimo piso creado de la lista*/
-    if (longitud(auxListaPisos)==1){
-        Piso* ptrPiso = (Piso*)primero(auxListaPisos)->ptrDato;
-         cout<<"longitud auxListaUbicacion: "<<longitud(ptrPiso->listaUbicacion)<<endl;
-
-        if (listaVacia(ptrPiso->listaUbicacion)){
-            adicionarPrincipio(ptrPiso->listaUbicacion,&ubicacion);
-            cout<<"principio100"<<endl;
-
-
-
-            }
-        else{
-            adicionarFinal(ptrPiso->listaUbicacion,&ubicacion);
-            cout<<"final"<<endl;
-
-        }
-       }
-    else{
-        Piso* ptrPiso = (Piso*)ultimo(auxListaPisos)->ptrDato;
-        //Piso piso= *ptrPiso;
-
-        if (listaVacia(ptrPiso->listaUbicacion)){
-            adicionarPrincipio(ptrPiso->listaUbicacion,&ubicacion);
-            cout<<"principio"<<endl;
-             //*ptrPiso= piso;
-            //  setListaPisos(estanteria,auxListaPisos);
-
-        }
-        else{
-            adicionarFinal(ptrPiso->listaUbicacion,&ubicacion);
-            cout<<"final"<<endl;
-             //*ptrPiso= piso;
-           //   setListaPisos(estanteria,auxListaPisos);
-
-            }
     }
 
+void crearEstanteria(Lista &listaEstanteria, int nroCalle){
 
-
+    Estanteria estanteria;
+    PtrDato ptrDato= construirEstanteria(estanteria);
+    setNroCalle(*((Estanteria*)ptrDato),nroCalle);
+    adicionarFinal(listaEstanteria,ptrDato);
 
 
 
 }
+
+
+
+
+
+
+
 
 
 
