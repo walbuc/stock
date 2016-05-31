18b767fb1fa6bf6e92288576155996421f5f572f
Dario <dariozenden@gmail.com>
2016-05-31 11:59:43 -0300

    agrego archivo adminCamiones.h

---
+++ stock/stock/AdminCamiones.h
@@ -0,0 +1,14 @@
+#ifndef AdminCamiones_h
+#define AdminCamiones_h
+
+#include <stdio.h>
+#include "funciones.h"
+#include "lista.h"
+
+#endif /* AdminCamiones_h */
+
+
+
+void cargarCamiones(Lista &listaSolicitudes, Lista &listaCamiones);
+
+void utilizacionCamiones(Lista &listaCamiones)
