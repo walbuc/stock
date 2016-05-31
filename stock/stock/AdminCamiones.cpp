0bf9932c4d8dfa3631cb72282f51415f9c8c925d
Dario <dariozenden@gmail.com>
2016-05-31 11:59:25 -0300

    agrego archivo adminCamiones.h

---
+++ stock/stock/AdminCamiones.cpp
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include "AdminCamiones.h"
+#include "fstream"
+#include <iostream>
+
+using namespace std;
+
+void cargarCamiones(Lista &listaSolicitudes, Lista &listaCamiones)
+{
+	PtrNodoLista cursorSolicitud = primero(listaSolicitudes); //cursor para recorrer las solicitudes
+	PtrNodoLista cursorCamion; //cursor para recorrer los camiones
+	float auxkg, kgtotal;
+	
+	//creacion del primer camion
+	Camion* camion = new Camion;
+	crearCamion(*camion);
+	setNroCamion(*camion, 1);
+	adicionarFinal(listaCamiones, camion); //agrega a la lista de camiones
+	cursorCamion = ultimo(listaCamiones); //trae el ultimo camion de la lista (primer camion)
+	
+	while (cursorSolicitud != fin()) { //lee la solicitud entrante
+		auxkg = getCantidadSolicitud(*(Solicitud*) cursorSolicitud->ptrdato; //kg de la solicitud
+		kgtotal = auxkg + getCargaTotal(*(Camion*) cursorCamion->ptrdato); //suma de la solicitud con la carga del camion 
+		if (kgtotal <= getKGcapacidad(*(Camion*) cursorCamion->ptrdato)){ // si no supera la capacidad total del camion actual
+			setCargaTotal(*(Camion*) cursorCamion->ptrdato, kgtotal); //guarda la carga en el camion
+			cursorSolicitud = siguiente(ListaSolicitudes, cursorSolicitud); //pasa al a siguiente solicitud
+		}
+		else{ // si no entra en el camion
+		//crea nuevo camion
+			Camion* camion = new Camion;
+			crearCamion(*camion);
+			setNroCamion(*camion, (getNroCamion(*(Camion*) cursorCamion->ptrdato)+1));//sumo 1 al id
+			adicionarFinal(listaCamiones, camion);
+			cursorCamion = ultimo(listaCamiones); //trae el ultimo camion de la lista (camion nuevo)
+			setCargaTotal(*(Camion*) cursorCamion->ptrdato, auxkg); //mete la carga de la solicitud que no entro en el camion anterior
+			cursorSolicitud = siguiente(ListaSolicitudes, cursorSolicitud); //pasa a la siguiente solicitud
+		}
+	}
+}
+
+//LISTAR CAMIONES EN PANTALLA
+/*
+void listarCamiones (Lista &listaCamiones) 
+{
+	PtrNodoLista cursorCamion = ultimo(listaCamiones); //cursor para recorrer los camiones
+	cout << "Nro Camion \t cargaTotal \t porcentaje Ocupacion" << endl;
+	while (cursorCamion != fin()) {
+		cout << getNroCamion(*(Camion*) cursorCamion->ptrdato) << "\t" << 
+		getCargaTotal(*(Camion*) cursorCamion->ptrdato) << "\t" << 
+		calcularPorcentajeOcupado(*(Camion*) cursorCamion->ptrdato) << endl;
+		cursorCamion = anterior(listaCamiones);
+		
+	}
+
+}
+*/
+void utilizacionCamiones(Lista &listaCamiones) {
+    PtrNodoLista cursorCamiones = ultimo(listaCamiones);
+	FILE *pf;
+    if ((pf = fopen("UtilizacionCamiones.txt", "w")) == NULL) {
+        cout << "Error al abrir el fichero de aplicaciones" << endl;
+    }
+
+    while (cursorCamiones != fin()) {
+        fprintf(pf, "%d;%.3f;%.2f\n", getNroCamion(*(Camion*) cursorCamion->ptrdato), getCargaTotal(*(Camion*) cursorCamion->ptrdato),
+            calcularPorcentajeOcupacion(*(Camion*) cursorCamion->ptrdato);
+
+        cursorCamion = anterior(listaCamion, cursorCamion);
+    }
+    fclose(pf);
+}
