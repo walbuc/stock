1afc5a76545270bed2bf4831acff816d7c2bbbcd
Buczacka Walter <wbuczacka@deviget.com>
2016-06-01 15:43:44 -0300

    agregar doc a admin articulos

--- stock/stock/AdminArticulos.h
+++ stock/stock/AdminArticulos.h
@@ -15,13 +15,35 @@
 #include "Deposito.h"
 
 
-
+/*
+ pre : lista Creada con crearLista().
+ post: carga instancias de articulos en memoria dinamica
+ relacionandolas en la lista.
+ 
+ lista : lista sobre la cual se cargan los articulos.
+ return void.
+ */
 void cargarArticulos(Lista &listaArticulos);
 
 
+/*
+ pre : lista Creada con crearLista(), deposito creado con construirDeposito().
+ post: carga el maestro de articulos y una lista indice de los articulos.
+ 
+ lista : lista sobre la cual se cargan los articulos.
+ lista : lista sobre la cual se cargan los indices.
+ deposito: sobre el cual se accede a las estanterias.
+ return void.
+ */
 void cargarMaestroDeArticulos(Deposito &deposito, Lista &listaArticulos, Lista &listaIndice);
 
-
+/*
+ pre : lista Creada con crearLista().
+ post: imprime el estado de la lista.
+ 
+ lista : lista sobre la cual se invoca la primitiva.
+ return void
+ */
 void estadoListaArticulos(Lista &listaArticulos);
 
 #endif /* AdminArticulos_h */
