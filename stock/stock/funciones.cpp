a1e03ea9f8f50d0f6d8a1627bb5e454826d1e0c9
gonzalezgustavo <gonzalezgustavojavier@gmail.com>
2016-06-02 20:00:56 -0300

    Actualizacion funciones.cpp

--- stock/stock/funciones.cpp
+++ stock/stock/funciones.cpp
@@ -3,7 +3,7 @@
 //  stock
 //
 //  Created by Walter Buczacka on 5/15/16.
-//  Copyright Â© 2016 Walter Buczacka. All rights reserved.
+//  Copyright © 2016 Walter Buczacka. All rights reserved.
 //
 
 
@@ -11,10 +11,12 @@
 #include "lista.h"
 #include "articulo.h"
 #include "estanteria.h"
+#include "Solicitud.h"
+#include "Camion.h"
 using namespace std;
 
 ResultadoComparacion compararDatoArticulo(PtrDato ptrDato1, PtrDato ptrDato2) {
-    
+
     if ( (*(Articulo*)ptrDato1).codigoArticulo > (*(Articulo*)ptrDato2).codigoArticulo ) {
         return MAYOR;
     }
@@ -36,7 +38,7 @@ ResultadoComparacion compararDatoIndice(PtrDato ptrDato1, PtrDato ptrDato2) {
     else {
         return IGUAL;
     }
-    
+
 }
 
 ResultadoComparacion compararDatoEstanteria(PtrDato ptrDato1, PtrDato ptrDato2) {
@@ -76,6 +78,31 @@ ResultadoComparacion compararDatoUbicacion(PtrDato ptrDato1, PtrDato ptrDato2) {
     }
 }
 
+ResultadoComparacion compararDatoSolicitud(PtrDato ptrDato1, PtrDato ptrDato2) {
+    if ( ((Solicitud*)ptrDato1)->idSolicitud > ((Solicitud*)ptrDato2)->idSolicitud ) {
+        return MAYOR;
+    }
+    else if (((Solicitud*)ptrDato1)->idSolicitud < ((Solicitud*)ptrDato2)->idSolicitud) {
+        return MENOR;
+    }
+    else {
+
+  return IGUAL;
+    }
+}
+ResultadoComparacion compararDatoCamion(PtrDato ptrDato1, PtrDato ptrDato2) {
+    if ( ((Camion*)ptrDato1)->nroCamion > ((Camion*)ptrDato2)->nroCamion ) {
+        return MAYOR;
+    }
+    else if (((Camion*)ptrDato1)->nroCamion < ((Camion*)ptrDato2)->nroCamion) {
+        return MENOR;
+    }
+    else {
+
+  return IGUAL;
+    }
+}
+
 
 
 
@@ -92,79 +119,79 @@ ResultadoComparacion compararDatoUbicacion(PtrDato ptrDato1, PtrDato ptrDato2) {
 /*de string a entero*/
 
 int cadenaAentero(string dato){
-    
+
     int num;
-    
+
     stringstream ss(dato);
-    
+
     ss >> num;
-    
+
     return num;
-    
+
 }
 
 float cadenaAfloat(string dato){
-    
+
     float num;
-    
+
     stringstream ss(dato);
-    
+
     ss >> num;
-    
+
     return num;
-    
+
 }
 
 /*de string a char*/
 
 char cadenaACaracter(string dato){
-    
+
     char num;
-    
+
     stringstream ss (dato);
-    
+
     ss >> num;
-    
+
     return num;
-    
+
 }
 
-//FunciÃ³n quita los espacios en blanco del string
+//Función quita los espacios en blanco del string
 
 string quitar_espacios(string cadena){
-    
+
     int inicio_pos;
-    
+
     int longitud;
-    
+
     int actual_pos = 0;
-    
-    
-    
+
+
+
     //Avanza en la cadena hasta que detecta un caracter valido
-    
+
     while(cadena[actual_pos] == ' ' || cadena[actual_pos] == '\n' || cadena[actual_pos] == '\t'){
-        
+
         actual_pos ++;
-        
+
     }
-    
+
     inicio_pos = actual_pos;
-    
-    
-    
+
+
+
     //Retrocede en la cadena hasta encontrar un caracter valido
-    
+
     actual_pos = cadena.size() - 1;
-    
+
     while(cadena[actual_pos] == ' ' || cadena[actual_pos] == '\n' || cadena[actual_pos] == '\t'){
-        
+
         actual_pos --;
-        
+
     }
-    
+
     longitud = actual_pos - inicio_pos + 1;
-    
+
     return cadena.substr(inicio_pos, longitud);
-    
+
 }
