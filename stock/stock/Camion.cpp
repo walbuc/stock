8d69c5a8876475e394698623efd8caff52798016
gonzalezgustavo <gonzalezgustavojavier@gmail.com>
2016-06-02 19:50:41 -0300

    Actualizacion Camion.cpp

--- stock/stock/Camion.cpp
+++ stock/stock/Camion.cpp
@@ -1,19 +1,17 @@
-#include "Camion.h";
+#include "Camion.h"
 
 /* Implementación de Primitivas */
 
 /*-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
 
-void crearCamion(Camion &amp;camion){
+void crearCamion(Camion &camion){
 
-	camion.nroCamion=0;	
-	camion.kgCapacidad=2500;	
-	camion.cargaTotal=0.0;		
-	camion.pilaSolicitudes;
+	camion.nroCamion=0;
+	camion.kgCapacidad=0;
+	camion.cargaTotal=0.0;
+	camion.porcentajeOcupado=0.0;
 }
-
 void destruirCamion(Camion &camion){}
-
 
 void setNroCamion(Camion &camion, int dato){
 
@@ -21,13 +19,11 @@ camion.nroCamion=dato;
 
 }
 
-
 void setKGcapacidad(Camion &camion, float dato){
 
 camion.kgCapacidad=dato;
 
 }
-
 
 void setCargaTotal(Camion &camion, float dato){
 
@@ -35,20 +31,11 @@ camion.cargaTotal=dato;
 
 }
 
-
-void setPilaSolicitudes(Camion &camion, Pila pilaNueva){
-
-camion.pilaSolicitudes=pilaNueva;
-
-}
-
-
 int getNroCamion(Camion &camion){
 
 return camion.nroCamion;
 
 }
-
 
 float getKGcapacidad(Camion &camion){
 
@@ -56,23 +43,23 @@ return camion.kgCapacidad;
 
 }
 
-
 float getCargaTotal(Camion &camion){
 
 return camion.cargaTotal;
 
 }
-
-
-Pila getPilaSolicitudes(Camion &camion){
-
-return camion.pilaSolicitudes;
-
+float getPorcentajeCarga(Camion &camion){
+return camion.porcentajeOcupado;
 }
+
+void setPorcentajeCarga(Camion &camion, float porcentaje){
+camion.porcentajeOcupado=porcentaje;
+}
+
 
 float calcularPorcentajeOcupado(Camion &camion){
 
-return ((camion.kgCapacidad * 100)/camion.cargaTotal);
+    return ((camion.cargaTotal * 100)/camion.kgCapacidad);
 
 }
 
