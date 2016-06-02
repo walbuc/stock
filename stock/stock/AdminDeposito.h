67541fce5678f91a1882093ebf4c4399c8ee309a
Dario <dariozenden@gmail.com>
2016-06-02 04:34:21 -0300

    actualizo archivo AdminDeposito.h

--- stock/stock/AdminDeposito.h
+++ stock/stock/AdminDeposito.h
@@ -1,8 +1,14 @@
 #ifndef ADMINDEPOSITO_H
 #define ADMINDEPOSITO_H
 #include "Deposito.h"
+#include "Estanteria.h"
+#include "Piso.h"
+#include "lista.h"
+#include "funciones.h"
 
 void configurarDeposito(Deposito &deposito);
 void imprimirDeposito(Deposito &deposito);
+void saldosDetallados (Deposito &deposito);
+void saldosAgrupados (Deposito &deposito);
 
 #endif // ADMINDEPOSITO_H
