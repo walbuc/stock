212d3f6fdb75f41440513973c9fcb8169ce64984
gonzalezgustavo <gonzalezgustavojavier@gmail.com>
2016-06-02 19:34:10 -0300

    Actuacilzacion AdminSolicitudes.cpp

--- stock/stock/AdminSolicitudes.cpp
+++ stock/stock/AdminSolicitudes.cpp
@@ -1,71 +1,113 @@
 
 #include <stdio.h>
 #include "AdminSolicitudes.h"
+#include "Solicitud.h"
 #include "cola.h"
 #include "fstream"
 #include <iostream>
 #include <queue>
+#include <windows.h>
 
 using namespace std;
 
-void cargarSolicitudes(Lista &listaSolicitudes)
-{
-    
-    ifstream fsolicitudes;
-    string dato;
-    
-    fsolicitudes.open("solicitudes.db");
-    
-    if (fsolicitudes.is_open())
-	{
 
-        while (fsolicitudes.good())
-		{
 
-            //new devuelve puntero al tipo de dato
-            Solicitud* ptrSolicitud = new Solicitud;
-            
-            constructor(*ptrSolicitud);
-            
-			//seteo codigo de Sucursal (int)
-            getline(fsolicitudes, dato,';');
-            setCodigoSucursal(*ptrSolicitud, cadenaAentero(dato));
+PtrNodoLista crearSolicitud(Lista &listaSolicitudes,int idSucursal, int idArticuloSucursal, float cantidadSucursal, float cantidadCumplida){
 
-			//seteo codigo de Articulo (int)
-			getline (fsolicitudes, dato,';');
-			setCodigoArticulo(*ptrSolicitud, cadenaAentero(dato));
 
-			//seteo la cantidad (float) de kg de la solicitud
-			getline (fsolicitudes, dato,';');
-            setCantidadSolicitud(*ptrSolicitud, cadenaAfloat(dato));
-            
-			//seteo la hora (string) de la solicitud
-			getline (fsolicitudes, dato,';');
-            setHoraSolicitud(*ptrSolicitud, dato);
+    Solicitud* punteroSolicitud= new Solicitud();
 
-			// En este lugar del codigo debo validad el stock de las solicitudes
+    constructorSolicitud(*punteroSolicitud);
+    //imprimirSolicitud(*punteroSolicitud);
+    PtrNodoLista cursor;
+    cursor=ultimo(listaSolicitudes);
 
-			//Agrego los datos de la solicitud a una lista de solicitudes
-            adicionarFinal(listaSolicitudes, ptrSolicitud);
-        }
-        
-        fsolicitudes.close();
-        
-    }
-    
-    else
-	{
-		cout << "Error al abrir el archivo." << endl << endl;
-        system ("PAUSE");
-    }
+    int id;
+
+
+
+   if (longitud(listaSolicitudes)==0){
+    id=0;
+   }else{
+       id= getIdSolicitud(*((Solicitud*)cursor->ptrDato));
+       //id=setIdSolicitud(*punteroSolicitud);
+       id=id+1;
+      // cout<< "id: "<<id<<endl;
+   }
+    setIdSolicitud(*punteroSolicitud, id);
+    setCodigoSucursal(*punteroSolicitud,idSucursal);
+    setCodigoArticulo(*punteroSolicitud, idArticuloSucursal);
+    setCantidadSolicitud(*punteroSolicitud, cantidadSucursal);
+    setCantidadCumplida(*punteroSolicitud, cantidadCumplida);
+    setHoraSolicitud(*punteroSolicitud,"");
+
+    PtrNodoLista ptrNodo= adicionarFinal(listaSolicitudes, punteroSolicitud);
+
+  return ptrNodo;
 
 }
 
+PtrNodoCola crearPendiente(Cola &colaPendiente, int idSucursal, int idArticuloSucursal, float cantidadSucursal, float cantidadCumplida){
 
-void cargarMaestroDeSolicitudes(Lista &listaSolicitudes, Lista &listaIndice) 
-{
+    Solicitud* punteroSolicitud= new Solicitud();
+    constructorSolicitud(*punteroSolicitud);
+    PtrNodoCola cursor;
+    cursor=colaFin(colaPendiente);
 
-	cargarSolicitudes(listaSolicitudes);  
+    int id;
+    if (colaVacia(colaPendiente)){
+    id=0;
+   }else{
+       id= getIdSolicitud(*((Solicitud*)cursor->ptrDato));
+       //id=setIdSolicitud(*punteroSolicitud);
+       id=id+1;
+      // cout<< "id: "<<id<<endl;
+   }
+    setIdSolicitud(*punteroSolicitud, id);
+    setCodigoSucursal(*punteroSolicitud,idSucursal);
+    setCodigoArticulo(*punteroSolicitud, idArticuloSucursal);
+    setCantidadSolicitud(*punteroSolicitud, cantidadSucursal);
+    setCantidadCumplida(*punteroSolicitud, cantidadCumplida);
+    setHoraSolicitud(*punteroSolicitud,"");
+
+   PtrNodoCola ptrNodo= encolar(colaPendiente, punteroSolicitud);
+return ptrNodo;
+}
+
+
+void estadoListaSolicitud(Lista &listaSolicitud) {
+
+    PtrNodoLista cursor;
+    cursor=primero(listaSolicitud);
+
+    cout<<"------------------------------"<<endl;
+    cout<<"Solicitudes Procesadas"<<endl;
+    cout<<"------------------------------"<<endl;
+
+    while (cursor != fin()){
+
+        imprimirSolicitud(*((Solicitud*)cursor->ptrDato));
+
+        cursor=siguiente(listaSolicitud,cursor);
+    }
+}
+void estadoColaPendiente(Cola &colaPendiente) {
+    PtrDato dato;
+    Cola auxCola;
+    crearCola(auxCola);
+     cout<<"------------------------------"<<endl;
+     cout<<"Solicitudes Pendientes"<<endl;
+     cout<<"------------------------------"<<endl;
+
+    while (colaFrente(colaPendiente)!= finCola()){
+
+
+
+        dato= desencolar(colaPendiente); //saca el dato
+        imprimirSolicitud(*((Solicitud*)dato));//imprime la solicitud
+        encolar(auxCola, dato);//guarda la solicitud en una cola auxiliar para no perderla
+     }
+    colaPendiente=auxCola; //sobreescribe la colaPendiente original
 
 }
 
@@ -74,28 +116,9 @@ void imprimirSolicitud(Solicitud &solicitud){
     cout<<"Cod. de Sucursal: "<<solicitud.codigoSucursal<<endl;
     cout<<"Cod. de Articulo: "<<solicitud.codigoArticulo<<endl;
     cout<<"Cantidad de Kg.: "<<solicitud.cantidad<<endl;
-    cout<<"Hora de la solicitud: "<<solicitud.hora<<endl;
+    cout<<"Cantidad cumplidad: "<<solicitud.cantidadCumplida<<endl;
     cout<<"--------------------------------------"<<endl;
 }
 
-bool existeArticulo()
-{
-	
-}
 
-/*verifica la existencia del articulo*/
-bool existeStockArticulo();
-
-/* si el articulo no cumple con la totalidad o la parcialidad
-de las 2 condiciones anteriores,
-carga el articulo en la cola de articulos pendientes */
-void agregarPendiente(Cola &colaPendientes, PtrDato ptrDato)
-{
-	
-
-}
-
-/* en caso contrario,
-carga el articulo en la lista de pedido */
-void agregarSolicitud();
 
