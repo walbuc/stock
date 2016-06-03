cfd15dab49b67bb134a5a6a72c71eadaa7848682
gonzalezgustavo <gonzalezgustavojavier@gmail.com>
2016-06-03 05:18:54 -0300

    actualizacionAdminDeposito.cpp y .h

--- stock/stock/AdminDeposito.h
+++ stock/stock/AdminDeposito.h
@@ -1,11 +1,15 @@
 #ifndef ADMINDEPOSITO_H
 #define ADMINDEPOSITO_H
 #include "Deposito.h"
+#include "Cola.h"
 
+string procesaString(string dato);
 void configurarDeposito(Deposito &deposito);
 void imprimirDeposito(Deposito &deposito);
 
 void saldosDetallados (Deposito &deposito);
 void saldosAgrupados (Deposito &deposito);
-
+void escribirFaltantes(Cola &colaPendiente);
+void escribirVentas(Lista &listaSolicitud);
+void utilizacionDeCamiones(Lista &listaCamiones);
 #endif // ADMINDEPOSITO_H
